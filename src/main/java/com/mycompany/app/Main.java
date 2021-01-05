89817d5d8e41974f406dccf646991ee404fadf09
Lucas <luaugusto@laposte.net>
2021-01-05 10:58:15 +0100

    changed the structure of the project to a maven project

---
+++ src/main/java/com/mycompany/app/Main.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import app.App;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        App app = new App();
+        app.run();
+        
+    }
+}
